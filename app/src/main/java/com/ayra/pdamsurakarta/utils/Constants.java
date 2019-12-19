c71f9ad89f7f049f52eabe11248219998301b4f6
AryaDharma <45990704+AyraDhrma@users.noreply.github.com>
2019-12-19 09:39:11 +0700

    Update Constants.java

--- app/src/main/java/com/ayra/pdamsurakarta/utils/Constants.java
+++ app/src/main/java/com/ayra/pdamsurakarta/utils/Constants.java
@@ -2,6 +2,6 @@ package com.ayra.pdamsurakarta.utils;
 
 public class Constants {
     public class apiKey{
-        public static final String apiKey = "4e57ea94d1e3fcf53a313b118b7767b2f8d07944";
+        public static final String apiKey = "";
     }
 }
